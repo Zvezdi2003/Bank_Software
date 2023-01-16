d79482c16b0e41ff1659af9e4b0c7378d6cadf3a
Zvezdelin Penkov <115587735+Zvezdi2003@users.noreply.github.com>
2023-01-16 22:52:13 +0200

    Update user-services.h

--- user-services.h
+++ user-services.h
@@ -2,6 +2,8 @@
 #define ENCODINGPASSWORD_USER_SERVICES_H
 
 #include <string>
+#include <iostream>
+#include <fstream>
 #include <vector>
 #include "user-data.h"
 #include "global-constants.h"
@@ -13,8 +15,70 @@ vector<UserData> users;
 bool isLogged = false;
 UserData loggedUser;
 
+int numbersAfterComma(string str) {
+	bool comma = false;
+	int counter = 0;
+	for (int i = 0; str[i] != '\0'; i++) {
+		if (comma) {
+			counter++;
+		}
+		if (str[i] == ',' || str[i] == '.') {
+			comma = true;
+		}
+	}
+	return  counter;
+}
+
+double stringToDouble(string str)
+{
+	double number = 0;
+
+	for (int i = 0; str[i] != '\0'; i++) {
+		if (str[i] == '.' || str[i] == ',') {
+			continue;
+		}
+		number = number * 10 + (str[i] - '0') * 10;
+	}
+	int digitsAfterComa = numbersAfterComma(str);
+	digitsAfterComa++;
+
+	while (digitsAfterComa > 0) {
+		number /= 10;
+		digitsAfterComa--;
+	}
+
+	return number;
+
+}
+
 void loadUsers() {
-	
+	std::ifstream file("users.txt");
+
+	if (!file.is_open()) {
+		cerr << "Error: unable to open file." << endl;
+		return;
+	}
+
+	string current;
+	getline(file, current, ':');
+
+	while (current != "\n" && !file.eof())
+	{
+
+		UserData user;
+
+		user.username = current;
+
+		getline(file, current, ':');
+		user.password = current;
+
+		getline(file, current);
+		user.balance = stringToDouble(current);
+
+		users.push_back(user);
+
+		getline(file, current, ':');
+	}
 }
 
 unsigned Hashing(string password)
